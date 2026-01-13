cb988b88e0a997ee390c1a22a32c50c18a253709
ponraj-hub <ponraj8785@gmail.com>
2026-01-13 05:52:03 +0530

    third python commit

--- PythonProject/hello.py
+++ PythonProject/hello.py
@@ -1,8 +1,19 @@
 print("hello world today")
 
+
 def write():
-    name = "Ponraj Natarajan"
-    print("My name is:", name)
+    Name = "Ponraj Natarajan"
+    Age=41
+    Profession="IT"
+    City="Chennai"
+    Marital_status="Married"
+
+    print("My name is:", Name)
+    print("My age is:", Age)
+    print("My profession is:", Profession)
+    print("My city is:", City)
+    print("My marital-status is:", Marital_status)
+
 
 write()
 
